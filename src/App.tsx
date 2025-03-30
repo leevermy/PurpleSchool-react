336dc6bc99ce614f9dcd24a5cb0af1ede2d593d8
Aleksey Goryanin <risnotr@gmail.com>
2025-03-30 11:45:41 +0300

    add title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import Button from './components/Button'
+import Description from './components/Description';
+import Title from './components/Title';
 
 function App() {
   return (
     <>
+      <div className='p-20 bg-gray-800'>
+        <Title>Поиск</Title>
+        <Description>
+          Введите название фильма, сериала или мультфильма для   поиска и добавления в избранное.
+        </Description>
+        <Button onClick={() => console.log('clicked')}>Искать</Button>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,7 +16,7 @@ const Button: React.FC<IButtonProps> = ({ children, disabled = false, onClick, c
 	return (
 		<button
 		onClick={disabled ? undefined : onClick}
-		className={`bg-violet-400 px-8 py-4 text-white hover:bg-violet-500 cursor-pointer transition duration-300 ${className}`}
+		className={`bg-violet-400 px-8 py-4 text-white rounded-xl hover:bg-violet-500 cursor-pointer transition duration-300 ${className}`}
 		>{children}</button>
 	)
 }
